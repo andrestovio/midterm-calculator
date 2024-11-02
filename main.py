0caeec6a0796cc76983dc6704191f05c758c1b0f
andrestovio <act@njit.edu>
2024-11-02 16:07:01 -0400

    Modified main.py to not have any functionality

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# Importing the "calculator" function from the "calculator.py" file located in the "app" folder.
-# This allows us to use the calculator functionality in our program.
-from app.calculator import calculator
+"""Temporary main.py"""
 
-# This block checks if the current file is being executed directly, rather than being imported by another file.
-# If it's being run directly, Python assigns the value "__main__" to the special variable "__name__".
-# This ensures that the following code will only execute when the file is run as a script.
 if __name__ == "__main__":
 
-    calculator()
+    print("Hello World")
