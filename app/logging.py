f7d51c179cf51efdbe9c5029c0fa59283ac6606a
andrestovio <act@njit.edu>
2024-11-08 21:34:35 -0500

    Implemented dotenv to logging.py file

--- app/logging.py
+++ app/logging.py
@@ -1,11 +1,17 @@
 import logging
+import os
+from dotenv import load_dotenv
+
+# Load environment
+load_dotenv()
 
 # Configure logging
+log_file = os.getenv("LOG_FILE", "default.log")
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("calculator.log"),
+        logging.FileHandler(log_file),
         logging.StreamHandler()
     ]
 )
